f3ee0c43b665260fe09e05a05dc01b9ec1711367
Maxwell Mullin <mullinmax@gmail.com>
2024-02-18 22:14:29 -0500

    cleanup

--- src/webui/routes/category_form.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectMultipleField
-from wtforms.validators import DataRequired, URL
-
-class CategoryForm(FlaskForm):
-    name = StringField('Category Name', validators=[DataRequired()])
-    submit = SubmitField('Add Category')
--- src/webui/routes/feed_form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, URL
-
-class FeedForm(FlaskForm):
-    name = StringField('Feed Name', validators=[DataRequired()])
-    url = StringField('RSS URL', validators=[DataRequired(), URL()])
-    category = StringField('Category', validators=[DataRequired()])
-    submit = SubmitField('Add Feed')
