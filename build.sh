5291309799ff02c870733f542f79e70f64b6ab40
Maxwell Mullin <mullinmax@gmail.com>
2024-03-14 18:47:46 -0400

    new build setup base -> shared

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # build.sh
 
-echo "Building blinder base image..."
-docker build -t blinder-base:latest ./src/shared
+echo "Building blinder shared image..."
+docker build -t blinder-shared:latest ./src/shared
 
 echo "Building ingest and webui services..."
 docker-compose up --build
