078e8caf85cbb9c1e663107dcd81176c25e3b8d1
Maxwell Mullin <mullinmax@gmail.com>
2024-04-03 21:41:44 -0400

    hello world working

---
+++ src/api/main.py
@@ -0,0 +1,14 @@
+# app.py
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info", reload=True)
