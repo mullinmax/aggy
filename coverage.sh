996396047f3ef3edffd49add34eebe2be8973d4e
Maxwell Mullin <mullinmax@gmail.com>
2024-05-07 21:55:33 -0400

    coverage actually writing

---
+++ coverage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+compose_file="src/api/docker-compose.yml"
+docker-compose -f $compose_file up --build -d
+echo "Waiting for the database to be ready..."
+export REDIS_HOST=dev-blinder-db
+export REDIS_PORT=6379
+export JWT_ALGORITHM=HS256
+export JWT_SECRET=429bceb2ab20f5785a4b609a725b0164be73a95d8ce04706ed8366cfe6ade896
+
+sleep 5
+
+echo "Database is ready - running tests now"
+
+pytest src/api/tests --cov=src/api --cov-report=xml:coverage.xml
+docker-compose -f $compose_file down
