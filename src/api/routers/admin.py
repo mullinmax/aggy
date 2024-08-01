4cd19ac7d9f74c94f2e2fe865855433af73fd8ae
Maxwell Mullin <mullinmax@gmail.com>
2024-07-31 22:43:43 -0400

    add infro for version route

--- src/api/routers/admin.py
+++ src/api/routers/admin.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter
 from fastapi.responses import RedirectResponse
+import re
+
+from config import config
+from route_models.version import Version
 
 admin_router = APIRouter()
 
@@ -8,3 +12,13 @@ admin_router = APIRouter()
 @admin_router.get("/", include_in_schema=False)
 async def root():
     return RedirectResponse(url="/docs", status_code=307)
+
+
+# route to get the current version of the API
+@admin_router.get("/version", response_model=Version)
+async def get_version():
+    version = config.get("BUILD_VERSION", "0.0.0-beta")
+
+    # clean up the version string
+    version = re.search(r"(\d+\.\d+\.\d+)-?beta?", version).group(1)
+    return Version(version=version)
