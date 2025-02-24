bbd1bf090ec7d2074479adf56c8c401576eff917
maxwell mullin <mullinmax@gmail.com>
2025-02-23 21:53:58 -0500

    make it a string first

--- src/api/ingest/item/open_graph.py
+++ src/api/ingest/item/open_graph.py
@@ -5,7 +5,7 @@ from db.item import ItemLoose
 
 
 def ingest_open_graph_item(item: ItemLoose) -> ItemLoose:
-    response = requests.get(item.url)
+    response = requests.get(str(item.url))
     soup = BeautifulSoup(response.text, "html.parser")
     og_tags = soup.find_all(
         "meta", attrs={"property": lambda x: x and x.startswith("og:")}
