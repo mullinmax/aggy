3341bfeb2716501147d282490aba2a3f13cf86bc
Maxwell Mullin <mullinmax@gmail.com>
2025-04-14 20:56:57 -0400

    commit what I left

--- src/api/entrypoint.sh
+++ src/api/entrypoint.sh
@@ -6,10 +6,19 @@ airflow db init
 airflow webserver & airflow scheduler &
 
 # Wait for Airflow to be ready
-sleep 30
+sleep 10
 
-# Run tests
-pytest -x /src/api/tests/
+
+airflow users  create --role Admin --username admin --email admin --firstname admin --lastname admin --password admin
+
+# if env argument is test:
+if [ "$1" = "test" ]; then
+    # Run tests
+    pytest -x /src/api/tests/
+    exit 0
+else
+    python /src/api/main.py
+fi
 
 # Keep the container running
 wait
